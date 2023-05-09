734fe24913099d43ecd352827f287988541ff17d
Kenny Weiss <kenny1weiss@aol.com>
2023-05-09 13:21:10 -0700

    Store user data in session

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, session
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 from flask_app.models import user
@@ -20,4 +20,21 @@ def register():
             "password": bcrypt.generate_password_hash(request.form["password"])
         }
         user.User.create(data)
-    return redirect(url_for("index"))
+        session["first_name"] = data["first_name"]
+        session["last_name"] = data["last_name"]
+        session["email"] = data["email"]
+        session["password"] = data["password"]
+
+    return "Success"
+
+@app.route("/login", methods=["POST"])
+def login():
+    if not user.User.is_valid(request.form):
+        return redirect(url_for("index"))
+    else:
+        data = {
+            "email": request.form["email"],
+            "password": bcrypt.generate_password_hash(request.form["password"])
+        }
+        user.User.create(data)
+    return redirect(url_for("index")) 

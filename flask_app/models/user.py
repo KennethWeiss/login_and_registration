f49479fae0b683e3e1e240516938febe2c3df476
Kenny Weiss <kenny1weiss@aol.com>
2023-05-09 11:22:50 -0700

    Add create to user model

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class User:
+    db = "login_registration"
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -9,4 +10,10 @@ class User:
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        
+
+    @classmethod
+    def create(cls,data):
+        query = """INSERT INTO user (first_name, last_name, email, password) 
+                VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"""
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return results

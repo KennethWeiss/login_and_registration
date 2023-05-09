e84f8cb927f307fd98cc30e87a23ad8b29779049
Kenny Weiss <kenny1weiss@aol.com>
2023-05-09 12:05:34 -0700

    Add register route

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,23 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, redirect, request, url_for
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask_app.models import user
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not user.User.is_valid(request.form):
+        return redirect(url_for("index"))
+    else:
+        data = {
+            "first_name": request.form["first_name"],
+            "last_name": request.form["last_name"],
+            "email": request.form["email"],
+            "password": bcrypt.generate_password_hash(request.form["password"])
+        }
+        user.User.create(data)
+    return redirect(url_for("index"))

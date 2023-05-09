29abb720d4792192821924b83da53f2676ed78d4
Kenny Weiss <kenny1weiss@aol.com>
2023-05-09 11:33:34 -0700

    Start validating for form

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,3 +1,4 @@
+from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class User:
@@ -17,3 +18,23 @@ class User:
                 VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"""
         results = connectToMySQL(cls.db).query_db(query,data)
         return results
+    
+    @staticmethod
+    def is_valid(user_dict):
+        is_valid = True
+        if len(user_dict["first_name"]) < 2:
+            is_valid = False
+            flash("First name should have at least 2 characters")
+        if len(user_dict["last_name"]) < 2:
+            is_valid = False
+            flash("Last name should have at least 2 characters")
+        if len(user_dict["email"]) < 2:
+            is_valid = False
+            flash("Email should have at least 2 characters")
+        if len(user_dict["password"]) < 2:
+            is_valid = False
+            flash("Password should have at least 2 characters")
+        if user_dict["password_confirmation"] != user_dict["password"]:
+            is_valid = False
+            flash("Password must match password confirmation")
+        return is_valid
